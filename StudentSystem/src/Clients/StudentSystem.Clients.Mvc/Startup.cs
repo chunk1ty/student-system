880000ed939c746085d6aeb9add51abb359b6997
chunk1ty <>
2018-07-28 14:32:16 +0300

    added empty projects

---
+++ StudentSystem/src/Clients/StudentSystem.Clients.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentSystem.Clients.Mvc.Startup))]
+namespace StudentSystem.Clients.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
